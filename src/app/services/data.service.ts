3f813a2a239e87c90372413dadc309c7ac8724be
mburkrb43 <mburkhart41@gmail.com>
2017-07-15 21:41:21 -0400

    CodeComplete

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,7 +6,8 @@ export class DataService {
   questions:Question[];
 
   constructor() {
-    this.questions = [
+
+    /*this.questions = [
       {
         text: 'What is your name?',
         answer: 'My name is Michael',
@@ -17,24 +18,48 @@ export class DataService {
         answer: 'Red',
         hide:true
       }
-    ];
+    ]; */
 
    }
    getQuestions(){
+     if( localStorage.getItem('questions') == null) {
+        this.questions = [];
+     }
+    else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
+
      return this.questions;
    }
+
    addQuestion(question:Question){
       this.questions.unshift(question);
+
+      //init local var
+      let questions;
+
+      if( localStorage.getItem('questions') == null) {
+        questions = [];
+        //push new question
+        questions.unshift(question);
+        localStorage.setItem('questions',JSON.stringify(questions))
+      }
+      else {
+        questions = JSON.parse(localStorage.getItem('questions')); //init local var
+        //add new question
+        questions.unshift(question);
+        localStorage.setItem('questions',JSON.stringify(questions));
+      }
+
     }
    removeQuestion(question:Question){
-     console.log("removing " + question.text);
      for(let i=0;i < this.questions.length;i++)
       {
           if(question.text ==  this.questions[i].text)
-            {
-              console.log("found match" + this.questions[i].text + " " + i);
-              
+            {               
               this.questions.splice(i,1);
+              localStorage.setItem('questions',JSON.stringify(this.questions)); //only set again because spliced out of thislquestions
+              
             }
       }
    }
